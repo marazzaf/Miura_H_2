c5f044afbea1352cb4cec27d5482da517b94c09b
Frederic Marazzato <marazzato@lsu.edu>
2022-12-22 09:59:20 -0600

    Changed the name of the files for the hyper.

--- hyperboloid/old/convergence.py
+++ hyperboloid/old/convergence.py
@@ -3,6 +3,8 @@
 
 from firedrake import *
 from firedrake.petsc import PETSc
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot as plt
 import sys
 
 # the coefficient functions
@@ -10,6 +12,8 @@ def p(phi):
   sq = inner(phi.dx(0), phi.dx(0))
   aux = 4 / (4 - sq )
   return conditional(gt(sq, Constant(4)), Constant(100), aux)
+  #return interpolate(conditional(lt(aux, Constant(1)), Constant(100), aux), UU)
+  #return 1 / (1 - 0.25 * inner(phi.dx(0), phi.dx(0)))
   
 def q(phi):
   sq = inner(phi.dx(1), phi.dx(1))
@@ -56,22 +60,43 @@ PETSc.Sys.Print('Laplace equation ok')
 #Writing our problem now
 phi = Function(V, name='solution')
 phi.vector()[:] = project(phi_l, V).vector()
+phi_t = TrialFunction(V)
 psi = TestFunction(V)
-
-#Newton bilinear form
+#bilinear form for linearization
 Gamma = (p(phi) + q(phi)) / (p(phi)*p(phi) + q(phi)*q(phi))
-a = Gamma * inner(p(phi) * phi.dx(0).dx(0) + q(phi)*phi.dx(1).dx(1), div(grad(psi))) * dx
+a = Gamma * inner(p(phi) * phi_t.dx(0).dx(0) + q(phi)*phi_t.dx(1).dx(1), div(grad(psi))) * dx
+#a = inner(p(phi) * phi_t.dx(0).dx(0) + q(phi)*phi_t.dx(1).dx(1), div(grad(psi))) * dx
 
 #penalty to impose Dirichlet BC
 h = CellDiameter(mesh)
 pen = 1e1 #1e1
-pen_term = pen/h**4 * inner(phi, psi) * ds
+pen_term = pen/h**4 * inner(phi_t, psi) * ds
 a += pen_term
 L = pen/h**4 * inner(phi_D, psi)  * ds
-a -= L
 
-# Solving with Newton method
-solve(a == 0, phi, solver_parameters={'snes_monitor': None, 'snes_max_it': 25})
+# Picard iteration
+tol = 1e-5 #1e-9
+maxiter = 50
+phi_old = Function(V) #for iterations
+for iter in range(maxiter):
+  #linear solve
+  A = assemble(a)
+  b = assemble(L)
+  pp = interpolate(p(phi), UU)
+  PETSc.Sys.Print('Min of p: %.3e' % pp.vector().array().min())
+  solve(A, phi, b, solver_parameters={'direct_solver': 'mumps'}) # compute next Picard iterate
+    
+  eps = sqrt(assemble(inner(div(grad(phi-phi_old)), div(grad(phi-phi_old)))*dx)) # check increment size as convergence test
+  PETSc.Sys.Print('iteration{:3d}  H2 seminorm of delta: {:10.2e}'.format(iter+1, eps))
+
+  if eps < tol:
+    break
+  phi_old.assign(phi)
+
+if eps > tol:
+  PETSc.Sys.Print('no convergence after {} Picard iterations'.format(iter+1))
+else:
+  PETSc.Sys.Print('convergence after {} Picard iterations'.format(iter+1))
 
 #Computing error
 err = sqrt(assemble(inner(div(grad(phi-phi_D)), div(grad(phi-phi_D)))*dx))
